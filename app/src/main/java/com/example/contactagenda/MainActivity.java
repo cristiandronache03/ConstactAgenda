8183c58a97dc5aa1b91faea8983e32d1d749d6d1
cristiandronache03 <cristiandronache03@gmail.com>
2023-03-02 14:11:22 +0200

    main activity

--- app/src/main/java/com/example/contactagenda/MainActivity.java
+++ app/src/main/java/com/example/contactagenda/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.contactagenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // move to new activity to add contact
+                Intent intent = new Intent(MainActivity.this, AddContact.class);
+                startActivity(intent);
+            }
+        });
     }
 }
