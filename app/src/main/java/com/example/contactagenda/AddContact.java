f6ec2d44e767b7ba236719b6216d7e576bbc70a6
cristiandronache03 <cristiandronache03@gmail.com>
2023-03-02 14:20:35 +0200

    add contact activity

--- app/src/main/java/com/example/contactagenda/AddContact.java
+++ app/src/main/java/com/example/contactagenda/AddContact.java
@@ -1,14 +1,75 @@
 package com.example.contactagenda;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddContact extends AppCompatActivity {
+
+    private ImageView profiveIv;
+    private EditText nameEt,phoneEt,emailEt,noteEt;
+
+    private FloatingActionButton fab;
+
+    String name, phone, email, note;
+
+    ActionBar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        actionBar = getSupportActionBar();
+
+        actionBar.setTitle("Add Contact");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        profiveIv = findViewById(R.id.profileIv);
+        nameEt = findViewById(R.id.nameEt);
+        phoneEt = findViewById(R.id.phoneEt);
+        emailEt = findViewById(R.id.emailEt);
+        noteEt = findViewById(R.id.noteEt);
+        fab = findViewById(R.id.fab);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+
+    }
+
+    private void saveData() {
+
+        name = nameEt.getText().toString();
+        phone = phoneEt.getText().toString();
+        email = emailEt.getText().toString();
+        note = noteEt.getText().toString();
+
+        if(name.isEmpty() || phone.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Please fill in the fields...",Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+        }
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
